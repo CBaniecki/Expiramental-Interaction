57d6ed409f65e5711dde7d17ff5abc4277c5f342
Clarissa Baniecki <cbaniecki@mcad.edu>
2022-01-20 14:47:50 -0600

    Update script.js

---
+++ one-point interaction/script.js
@@ -0,0 +1,10 @@
+function setup () {
+    createCanvas(800,800);
+    background("#DC143C");
+}
+
+function draw(){
+    fill(255);
+    rect(200,100, 100,100);
+    rect.mouseOver(300,100,100,100);
+}
