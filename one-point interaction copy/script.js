c08029318d02e976606209b641fb18382aed422e
Clarissa Baniecki <cbaniecki@mcad.edu>
2022-01-23 15:14:21 -0600

    updated variables

--- one-point interaction copy/script.js
+++ one-point interaction copy/script.js
@@ -7,16 +7,16 @@ function preload() {
 
 function setup(){
     createCanvas(10,10);
-    //var spending = money.purchases[0].month
 
     var spending = money.purchases;
 
     for (var i = 0; i < spending.length; i++) {
-        createElement('h1', spending[i].store);
-        var month = spending[i].month; 
-        for( var j = 0; j< month.length; j++){
-           createDiv(month[j]);
+        createElement('h1', spending[i].month);
+        var store = spending[i].store; 
+        for( var j = 0; j< store.length; j++){
+           createDiv(store[j]);
         }
+
     }
 
  
