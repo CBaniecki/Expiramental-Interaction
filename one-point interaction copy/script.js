87feced2de3c8f97b1e9026fcb1bc6c53b8c1ec7
Clarissa Baniecki <cbaniecki@mcad.edu>
2022-01-23 14:49:16 -0600

    Update script.js

--- one-point interaction copy/script.js
+++ one-point interaction copy/script.js
@@ -1,17 +1,30 @@
 
+var money;
 
 function preload() {
-    spending = loadJSON("spending.json");
+    money = loadJSON("spending.json");
 }
 
 function setup(){
-    createCanvas(500,500);
+    createCanvas(10,10);
+    //var spending = money.purchases[0].month
+
+    var spending = money.purchases;
+
+    for (var i = 0; i < spending.length; i++) {
+        createElement('h1', spending[i].store);
+        var month = spending[i].month; 
+        for( var j = 0; j< month.length; j++){
+           createDiv(month[j]);
+        }
+    }
+
+ 
+
 
 }
 
 function draw(){
-    background(0);
-
-    fill(spending.r, spending.g, spending.b);
-    text(spending.store, 10,50);
+    background(255); 
+    fill(0);
 }
