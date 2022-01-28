4630b91109ba0563d373713632a9b9687084adfb
Clarissa Baniecki <cbaniecki@mcad.edu>
2022-01-27 21:58:12 -0600

    copied files for upload to MCAD server

---
+++ one-point-interaction-draft-one/sketch.js
@@ -0,0 +1,32 @@
+var money;
+
+
+function preload(){
+    money = loadJSON("spending.json");
+}
+
+
+
+function setup(){
+    createP()
+    noCanvas()
+    
+    var spending = money.purchases;
+
+    for(var i = 0; i < spending.length; i++){
+        createElement('h1', spending[i].month);
+        var store = spending[i].store;
+        for(var j = 0; j< store.length; j++ ){
+            createDiv(store[j]);
+        }
+    }
+
+}
+
+function draw(){
+
+var spending = money.purchases;
+    spending.pop(1);
+     
+}
+
