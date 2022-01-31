3433f46278fe82785033b79c53d1e7d0ec29686c
Clarissa Baniecki <cbaniecki@mcad.edu>
2022-01-31 15:49:04 -0600

    Draft Two files

---
+++ onepointinteraction-draft-two-one/sketch.js
@@ -0,0 +1,38 @@
+function setup(){
+    createCanvas(900,850);
+}
+
+function draw(){
+    background(0);
+
+    textSize(20);
+    text('december', 600,130);
+    fill(255);
+
+    textSize(15);
+    text('word search books', 500, 200);
+
+    textSize(15);
+    text('lavender lotion', 700, 200);
+
+
+    textSize(20);
+    text('october',100,130);
+
+    textSize(15);
+    text('picture frames', 60, 200);
+
+
+
+    textSize(20);
+    text('november', 100, 500);
+
+    textSize(15);
+    text('texas tech shirt', 50, 570);
+
+    textSize(15);
+    text('pandora charm', 200, 570);
+
+    
+
+}
