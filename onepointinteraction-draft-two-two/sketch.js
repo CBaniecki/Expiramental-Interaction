a02b2eae763356c9ec17159c7dc7f2b08fbf80e9
Clarissa Baniecki <cbaniecki@mcad.edu>
2022-02-01 12:53:53 -0600

    deleted data from JS file

--- onepointinteraction-draft-two-two/sketch.js
+++ onepointinteraction-draft-two-two/sketch.js
@@ -1,42 +1,17 @@
+var slider;
+
 function setup(){
-    createCanvas(900,850);
+    createCanvas(650,600);
+
+    slider = createSlider(0, 100, 0);
 }
 
 function draw(){
     background(0);
+    
+    //fill(255);
+    text('savings', 100,slider.value());
 
-    textSize(20);
-    text('december', 600,130);
-    fill(255);
-
-    textSize(15);
-    text('word search books', 500, 200);
-
-    textSize(15);
-    text('lavender lotion', 700, 200);
-
-    textSize(15);
-    text('gift cards', 500, 270);
-
-
-    textSize(20);
-    text('october',100,130);
-
-    textSize(15);
-    text('picture frames', 60, 200);
-
-    textSize(15);
-    text('printer ink', 200, 200);
-
-
-
-    textSize(20);
-    text('november', 100, 500);
-
-    textSize(15);
-    text('texas tech shirt', 50, 570);
-
-    textSize(15);
-    text('pandora charm', 200, 570);
-
+    rect(0,130,200,200)
+    fill("#ADD8E6");
 }
