c598e69f88608918b79fc6fd77c31aa423a7fd30
Clarissa Baniecki <cbaniecki@mcad.edu>
2022-02-08 19:13:00 -0600

    added images to display array, added time

--- onepointinteraction-draft-two-updated/sketch.js
+++ onepointinteraction-draft-two-updated/sketch.js
@@ -15,10 +15,12 @@ var index = 0;
 //var display = []
 
 var time1 = 2000;
-var time2 = 1000;
+//var time2 = 1000;
 
 
 var items = [];
+
+var display = [];
 
 function preload(){
     items[0] = loadImage('images/teddybear.jpeg');
@@ -26,6 +28,8 @@ function preload(){
     items[2] = loadImage('images/jacket.jpeg');
     items[3] = loadImage('images/caramel-popcorn.jpeg');
     items[4] = loadImage('images/licorice.jpeg');
+    items[5] = loadImage('images/reciept.jpg');
+    items[6] = loadImage('images/lip-balm.jpeg');
 }
 
 
@@ -42,7 +46,7 @@ function draw(){
     var currentTime = millis();
 
 
-    //if(mouseIsPressed=== true){
+    //if(mouseIsPressed === true){
     //  value = 0;
     //}
 
@@ -62,38 +66,53 @@ function draw(){
     //rect( 455, 320, 160, 75);
 
 
-    fill(255);
-    rect(20, 40, 190,450);
+    image(items[0], 500,310,150,130);
+    image(items[1], 600, 230,150,140);
+    image(items[2], 650, 353,200,280);
+    image(items[3], 410, 440, 240,200);
+    image(items[4], 340, 150, 260, 160);
+    image(items[5], 40, 30, 220, 700);
+    image(items[6], 600, 100, 250, 150);
 
-   
+
+    //fill(255);
+    //rect(40, 100, 210,500);
+
+
 
     fill(0);
     noStroke()
     textSize(20);
 
-    image(items[0], 500,260,150,130);
-    image(items[1], 590,190,150,140);
-    image(items[2], 650, 313,180,270);
-    image(items[3], 410, 390, 240,200);
-    image(items[4], 340, 100, 260, 160);
-    //image()
-
-    //text(products[0], 395, 260);
-    //text(products[2], 250, 160);
-    //text(products[1], 250, 260);
-    //text(products[3], 390, 160);
-    //text(products[4], 524, 160);
-    //text(products[5], 530, 260);
-    //text(products[6], 250, 365);
-    //text(products[7], 490, 365);
+    text('Impulsive Target Buys', 54, 160);
 
 
-    text('Total', 40, 430)
-    text('$34.35',120, 430)
+    textSize(15);
 
-  
-   if(currentTime >  time2){
-    text('Blanket', 40, 100)
+    text('click to reflect items on reciept', 50, 220);
+    text(display[0], 60, 250);
+    text(display[1], 200, 250);
+    text(display[2], 60, 275);
+    text(display[3], 200, 275);
+    text(display[4], 60, 300);
+    text(display[5], 200, 300);
+    text(display[6], 60, 325);
+    text(display[7], 200, 325);
+    text(display[8], 60, 350);
+    text(display[9], 200,350);
+    text(display[10], 60, 375);
+    text(display[11], 200, 375);
+    text(display[12], 60, 400);
+    text(display[13], 200, 400);
+
+
+    //text('Total', 40, 430)
+
+   if(currentTime > time1){
+        text('$34.35',200, 432);
+   }
+    //if(currentTime >  time2){
+    //text('Blanket', 40, 100)
     //text('$10.00', 140, 100)
     //text('Candy', 40, 130)
     //text('$1.00', 140, 130)
@@ -113,13 +132,23 @@ function draw(){
 
     //  text(products[0], 200, 100);
     //    text(products[1], 200, 200);
-
-
-    }
+    //}
 }
 
 
 function mousePressed(){
+    display.push('Blanket')
+    display.push("$$")
+    display.push("Candy")
+    display.push("$")
+    display.push("Jacket")
+    display.push("$$")
+    display.push("Plushie")
+    display.push("$")
+    display.push("Lip Balm")
+    display.push("$")
+    display.push("Caramel Popcorn")
+    display.push("$")
 }
 
 
