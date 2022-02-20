173f042e8e0c5a784179d54b2d2872f8f1a72573
Clarissa Baniecki <cbaniecki@mcad.edu>
2022-02-20 15:51:11 -0600

    proof of concept copy files

---
+++ codeforfutureidentities-proof-of-concept-copy/sketch.js
@@ -0,0 +1,34 @@
+let mic;
+
+function setup(){
+ let cnv=createCanvas(800,800);
+ cnv.mousePressed(userStartAudio);
+ mic = new p5.AudioIn();
+ mic.start();
+}
+
+function draw() {
+    background(0)
+    var vol = mic.getLevel();
+
+    stroke(255);
+    //fill(vol*255);
+    noFill();
+    ellipse(550, 200, 240, 240);
+    strokeWeight(vol*255);
+    ellipse(100, vol*200, 300,300);
+    ellipse(200, 400, 300,300);
+    ellipse(650,540, 240,240);
+    //console.log(vol);
+
+    //for (let i = 0; i< 6; i++) {
+    //      stroke(255);
+    //      ellipse( i*60 + 40, vol* 300+ random(50), 50);
+    //}
+
+   
+}
+
+function mousePressed(){
+    userStartAudio
+}
