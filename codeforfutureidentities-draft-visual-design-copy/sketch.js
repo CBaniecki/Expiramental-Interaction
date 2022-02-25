80cf945fcc2d4967b6a95801a5773ece2de6d27a
Clarissa Baniecki <cbaniecki@mcad.edu>
2022-02-24 21:27:44 -0600

    edited vidual idea two files for code for future identities

---
+++ codeforfutureidentities-draft-visual-design-copy/sketch.js
@@ -0,0 +1,57 @@
+
+
+
+
+function setup(){
+ cnv = createCanvas(1400,800);
+ 
+}
+
+function draw() {
+    background(0)
+ 
+    stroke(255);
+    fill(255);
+    ellipse(80, 40, 15, 15);
+    ellipse(100, 65, 15, 15);
+    ellipse(120, 100, 15, 15);
+    ellipse(150, 130, 15, 15);
+    ellipse(180, 160, 15, 15);
+    ellipse(210, 190, 15, 15);
+    ellipse(240, 220, 15, 15);
+    ellipse(280, 250, 15, 15);
+    ellipse(330, 280, 15, 15);
+    ellipse(380, 310, 15, 15);
+    ellipse(440, 340, 15, 15);
+    ellipse(500, 370, 15, 15);
+    ellipse(560, 400, 15, 15);
+    ellipse(630, 430, 15, 15);
+    ellipse(690, 460, 15, 15);
+    ellipse(750, 490, 15, 15);
+
+
+    ellipse(750, 240, 15, 15);
+    ellipse(800, 280, 15, 15);
+    ellipse(870, 320, 15, 15);
+    ellipse(940, 360, 15, 15);
+    ellipse(1010, 400, 15, 15);
+    ellipse(1070, 440, 15, 15);
+    ellipse(1130, 480, 15, 15);
+    ellipse(1190, 520, 15, 15);
+
+    ellipse(940, 60, 15, 15);
+    ellipse(1000, 100, 15, 15);
+    ellipse(1060, 140, 15, 15);
+    ellipse(1120, 180, 15, 15);
+    ellipse(1180, 220, 15, 15);
+
+    ellipse(50, 440, 15, 15);
+    ellipse(90, 500, 15, 15);
+    ellipse(130, 550, 15, 15);
+    ellipse(170, 600, 15, 15);
+    ellipse(220, 650, 15, 15);
+    ellipse(270, 700, 15, 15);
+    ellipse(320, 750, 15, 15);
+   
+}
+
