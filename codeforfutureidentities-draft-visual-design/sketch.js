4d6484c88423d8ff4ab55a9ecca2e339bb09e6a5
Clarissa Baniecki <cbaniecki@mcad.edu>
2022-02-24 21:27:03 -0600

    visual design files for code for future identities

---
+++ codeforfutureidentities-draft-visual-design/sketch.js
@@ -0,0 +1,42 @@
+
+
+
+
+function setup(){
+ cnv = createCanvas(1400,800);
+ 
+}
+
+function draw() {
+    background(0)
+ 
+    stroke(255);
+    noFill();
+    ellipse(300, 200, 300, 300);
+    ellipse(300, 200, 200, 200);
+    ellipse(300, 200, 100, 100);
+
+    ellipse(500, 400, 400, 400);
+    ellipse(500, 400, 300, 300);
+    ellipse(500, 400, 200, 200);
+    ellipse(500, 400, 100, 100);
+
+    ellipse(860, 300, 340, 340);
+    ellipse(860, 300, 240, 240);
+    ellipse(860, 300, 140, 140);
+
+    ellipse(1050, 500, 400, 400);
+    ellipse(1050, 500, 300, 300);
+    ellipse(1050, 500, 200, 200);
+    ellipse(1050, 500, 100, 100);
+
+    ellipse(1160, 180, 300, 300);
+    ellipse(1160, 180, 200, 200);
+    ellipse(1160, 180, 100, 100);
+
+    ellipse(180, 460, 300, 300);
+    ellipse(180, 460, 200, 200);
+    ellipse(180, 460, 100, 100);
+   
+}
+
